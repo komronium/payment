eb3fbc44e9da37b843c3376f4cff08f301d3f398
Komron <komronium@gmail.com>
2025-10-30 23:19:50 +0500

    fix url orders

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.payments.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='schema-swagger-ui'),
+    path('', include('apps.payments.urls')),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='schema-redoc'),
 ]
