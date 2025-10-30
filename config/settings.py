924eb5a2b999c5e4456a6a8dd33bf85fd6ddb9b7
Komron <komronium@gmail.com>
2025-10-30 23:11:01 +0500

    allowed hosts set to all

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8#hyrc@cj5_v*djgxxx2)afij5xrb$u^x)=#pmj)qk(fd1)g+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
